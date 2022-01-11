aa9489e5e2610877acc154d4326e6e7772e9467a
Trived22 <97497209+Trived22@users.noreply.github.com>
2022-01-11 11:28:06 +0530

    Update 1twosum.py

--- 1twosum.py
+++ 1twosum.py
@@ -7,13 +7,5 @@ class Solution:
                 return [a[sub],i]
             else :
                 a[curval] = i
-            class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        a={}
-        for i,curval in enumerate(nums) :
-            sub = target - curval
-            if sub in a:
-                return [a[sub],i]
-            else :
-                a[curval] = i
+            
             
